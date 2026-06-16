4332d5350186215aa6fe0ad378f809104793c780
Phyll1s0 <louhao088@sjtu.edu.cn>
2026-06-16 20:39:56 +0800

    feat: pass basic test

---
+++ .sim-runtime-cache/debug/deps/basic-fc4fea563640fdeb.d
@@ -0,0 +1,16 @@
+/home/louhao/chaos/.sim-runtime-cache/debug/deps/basic-fc4fea563640fdeb.d: tests/basic/main.rs tests/basic/group_01.rs tests/basic/group_02.rs tests/basic/group_03.rs tests/basic/group_04.rs tests/basic/group_05.rs tests/basic/group_06.rs tests/basic/group_07.rs tests/basic/group_08.rs tests/basic/group_09.rs tests/basic/group_10.rs tests/basic/group_11.rs
+
+/home/louhao/chaos/.sim-runtime-cache/debug/deps/basic-fc4fea563640fdeb: tests/basic/main.rs tests/basic/group_01.rs tests/basic/group_02.rs tests/basic/group_03.rs tests/basic/group_04.rs tests/basic/group_05.rs tests/basic/group_06.rs tests/basic/group_07.rs tests/basic/group_08.rs tests/basic/group_09.rs tests/basic/group_10.rs tests/basic/group_11.rs
+
+tests/basic/main.rs:
+tests/basic/group_01.rs:
+tests/basic/group_02.rs:
+tests/basic/group_03.rs:
+tests/basic/group_04.rs:
+tests/basic/group_05.rs:
+tests/basic/group_06.rs:
+tests/basic/group_07.rs:
+tests/basic/group_08.rs:
+tests/basic/group_09.rs:
+tests/basic/group_10.rs:
+tests/basic/group_11.rs:
