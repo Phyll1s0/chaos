a307af0eb14311e42206680f3bb4f5cecab229d8
Phyll1s0 <louhao088@sjtu.edu.cn>
2026-06-17 23:25:02 +0800

    chore: remove sim runtime cache

--- .sim-runtime-cache/debug/deps/chaos_tests-b3b2e40d5fdaa8c7.d
+++
@@ -1,7 +0,0 @@
-/home/louhao/chaos/.sim-runtime-cache/debug/deps/chaos_tests-b3b2e40d5fdaa8c7.d: src/lib.rs
-
-/home/louhao/chaos/.sim-runtime-cache/debug/deps/libchaos_tests-b3b2e40d5fdaa8c7.rlib: src/lib.rs
-
-/home/louhao/chaos/.sim-runtime-cache/debug/deps/libchaos_tests-b3b2e40d5fdaa8c7.rmeta: src/lib.rs
-
-src/lib.rs:
